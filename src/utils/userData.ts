10345a3a88a8460c2a7e726888412b2bbdadb3cd
Gabriel Carriel <ga123carriel2015@gmail.com>
2023-03-31 10:58:38 -0300

    fix: customing my infos

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+    nameUser: "Gabriel Carriel",
+    githubUser: "gaacarriel",
+    linkedinUser: "gabriel-carriel",
+    whatsappNumber: "+5515991094927",
+    emailUser: "ga123carriel2015@gmail.com",
+    instagramUser: "gaacarriel",
+    facebookUser: "",
 };
